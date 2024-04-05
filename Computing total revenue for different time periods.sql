c5492e6d43a075eed1a585bbb0706f7f1d59f2e1
Mythily Ramanathan <123518126+mythilyram@users.noreply.github.com>
2024-04-04 21:54:52 -0400

    Update Computing total revenue for different time periods.sql

--- Computing total revenue for different time periods.sql
+++ Computing total revenue for different time periods.sql
@@ -17,5 +17,435 @@ First, we'll get to know the database we're going to work with.
 The Northwind database
 We'll be using a Microsoft database named Northwind. 
   It describes a fictional store, its customers, and its orders. The original database is available here. 
-  We've modified it a bit to better suit the needs of this course.
+  We have modified it a bit to better suit the needs of this course.
+
+  
+The employees table
+
+  The employees table stores information about the people employed at Northwind. 
+  Each employee has a unique ID (stored in the column employee_id), a first and last name 
+  (first_name, last_name) and a professional title (title). 
+  There are also other columns in this table, such as hire_date and address.
+
+  employee_id	last_name	first_name	title	birth_date	hire_date	address	city	region	postal_code	country	reports_to
+1	Davolio	Nancy	Sales Representative	1968-12-08	2012-05-01	507 - 20th Ave. E. Apt. 2A	Seattle	WA	98122	USA	2
+2	Fuller	Andrew	Vice President, Sales	1972-02-19	2012-08-14	908 W. Capital Way	Tacoma	WA	98401	USA	null
+3	Smith	John	Sales Representative	1983-08-30	2012-04-01	722 Moss Bay Blvd.	Kirkland	WA	98033	USA	2
+4	Peacock	Margaret	Sales Representative	1957-09-19	2013-05-03	4110 Old Redmond Rd.	Redmond	WA	98052	USA	2
+5	Buchanan	Steven	Sales Manager	1975-03-04	2013-10-17	14 Garrett Hill	London	null	SW1 8JR	UK	2
+6	Suyama	Michael	Sales Representative	1983-07-02	2013-10-17	Coventry House Miner Rd.	London	null	EC2 7JR	UK	5
+7	King	Robert	Sales Representative	1980-05-29	2014-01-02	Edgeham Hollow Winchester Way	London	null	RG1 9SP	UK	5
+8	Callahan	Laura	Inside Sales Coordinator	1978-01-09	2014-03-05	4726 - 11th Ave. N.E.	Seattle	WA	98105	USA	2
+9	Dodsworth	Anne	Sales Representative	1986-01-27	2014-11-15	7 Houndstooth Rd.	London	null	WG2 7LT	UK	5
+10	Smith	John	Sales Representative	1994-08-30	2017-03-21	22 Abbey Rd	London	null	NW6 5JG	UK	2
+
+The customers table
+
+Each customer has a 5-letter customer_id column and a company_name column. 
+  There are also columns related to the contact person (contact_name, contact_title) 
+  and columns related to the customer''s address and fax.
+
+Exercise
+For each customer select customer_id, company_name, contact_name, contact_title, and city.  
+
+customer_id	company_name	contact_name	contact_title	city
+ALFKI	Alfreds Futterkiste	Maria Anders	Sales Representative	Berlin
+ANATR	Ana Trujillo Emparedados y helados	Ana Trujillo	Owner	México D.F.
+ANTON	Antonio Moreno Taquería	Antonio Moreno	Owner	México D.F.
+AROUT	Around the Horn	Thomas Hardy	Sales Representative	London
+BERGS	Berglunds snabbköp	Christina Berglund	Order Administrator	Luleå
+BLAUS	Blauer See Delikatessen	Hanna Moos	Sales Representative	Mannheim
+BLONP	Blondesddsl père et fils	Frédérique Citeaux	Marketing Manager	Strasbourg
+BOLID	Bólido Comidas preparadas	Martín Sommer	Owner	Madrid
+BONAP	Bon app'	Laurence Lebihan	Owner	Marseille
+BOTTM	Bottom-Dollar Markets	Elizabeth Lincoln	Accounting Manager	Tsawassen
+BSBEV	B's Beverages	Victoria Ashworth	Sales Representative	London
+CACTU	Cactus Comidas para llevar	Patricio Simpson	Sales Agent	Buenos Aires
+CENTC	Centro comercial Moctezuma	Francisco Chang	Marketing Manager	México D.F.
+CHOPS	Chop-suey Chinese	Yang Wang	Owner	Bern
+COMMI	Comércio Mineiro	Pedro Afonso	Sales Associate	Sao Paulo
+CONSH	Consolidated Holdings	Elizabeth Brown	Sales Representative	London
+DRACD	Drachenblut Delikatessen	Sven Ottlieb	Order Administrator	Aachen
+DUMON	Du monde entier	Janine Labrune	Owner	Nantes
+EASTC	Eastern Connection	Ann Devon	Sales Agent	London
+ERNSH	Ernst Handel	Roland Mendel	Sales Manager	Graz
+FAMIA	Familia Arquibaldo	Aria Cruz	Marketing Assistant	Sao Paulo
+FISSA	FISSA Fabrica Inter. Salchichas S.A.	Diego Roel	Accounting Manager	Madrid
+FOLIG	Folies gourmandes	Martine Rancé	Assistant Sales Agent	Lille
+FOLKO	Folk och fä HB	Maria Larsson	Owner	Bräcke
+FRANK	Frankenversand	Peter Franken	Marketing Manager	München
+FRANR	France restauration	Carine Schmitt	Marketing Manager	Nantes
+FRANS	Franchi S.p.A.	Paolo Accorti	Sales Representative	Torino
+FURIB	Furia Bacalhau e Frutos do Mar	Lino Rodriguez	Sales Manager	Lisboa
+GALED	Galería del gastrónomo	Eduardo Saavedra	Marketing Manager	Barcelona
+GODOS	Godos Cocina Típica	José Pedro Freyre	Sales Manager	Sevilla
+GOURL	Gourmet Lanchonetes	André Fonseca	Sales Associate	Campinas
+GREAL	Great Lakes Food Market	Howard Snyder	Marketing Manager	Eugene
+GROSR	GROSELLA-Restaurante	Manuel Pereira	Owner	Caracas
+HANAR	Hanari Carnes	Mario Pontes	Accounting Manager	Rio de Janeiro
+HILAA	HILARION-Abastos	Carlos Hernández	Sales Representative	San Cristóbal
+HUNGC	Hungry Coyote Import Store	Yoshi Latimer	Sales Representative	Elgin
+HUNGO	Hungry Owl All-Night Grocers	Patricia McKenna	Sales Associate	Cork
+ISLAT	Island Trading	Helen Bennett	Marketing Manager	Cowes
+KOENE	Königlich Essen	Philip Cramer	Sales Associate	Brandenburg
+LACOR	La corne d'abondance	Daniel Tonini	Sales Representative	Versailles
+LAMAI	La maison d'Asie	Annette Roulet	Sales Manager	Toulouse
+LAUGB	Laughing Bacchus Wine Cellars	Yoshi Tannamuri	Marketing Assistant	Vancouver
+LAZYK	Lazy K Kountry Store	John Steel	Marketing Manager	Walla Walla
+LEHMS	Lehmanns Marktstand	Renate Messner	Sales Representative	Frankfurt a.M.
+LETSS	Let's Stop N Shop	Jaime Yorres	Owner	San Francisco
+LILAS	LILA-Supermercado	Carlos González	Accounting Manager	Barquisimeto
+LINOD	LINO-Delicateses	Felipe Izquierdo	Owner	I. de Margarita
+LONEP	Lonesome Pine Restaurant	Fran Wilson	Sales Manager	Portland
+MAGAA	Magazzini Alimentari Riuniti	Giovanni Rovelli	Marketing Manager	Bergamo
+MAISD	Maison Dewey	Catherine Dewey	Sales Agent	Bruxelles
+MEREP	Mère Paillarde	Jean Fresnière	Marketing Assistant	Montréal
+MORGK	Morgenstern Gesundkost	Alexander Feuer	Marketing Assistant	Leipzig
+NORTS	North/South	Simon Crowther	Sales Associate	London
+OCEAN	Océano Atlántico Ltda.	Yvonne Moncada	Sales Agent	Buenos Aires
+OLDWO	Old World Delicatessen	Rene Phillips	Sales Representative	Anchorage
+OTTIK	Ottilies Käseladen	Henriette Pfalzheim	Owner	Köln
+PARIS	Paris spécialités	Marie Bertrand	Owner	Paris
+PERIC	Pericles Comidas clásicas	Guillermo Fernández	Sales Representative	México D.F.
+PICCO	Piccolo und mehr	Georg Pipps	Sales Manager	Salzburg
+PRINI	Princesa Isabel Vinhos	Isabel de Castro	Sales Representative	Lisboa
+QUEDE	Que Delícia	Bernardo Batista	Accounting Manager	Rio de Janeiro
+QUEEN	Queen Cozinha	Lúcia Carvalho	Marketing Assistant	Sao Paulo
+QUICK	QUICK-Stop	Horst Kloss	Accounting Manager	Cunewalde
+RANCH	Rancho grande	Sergio Gutiérrez	Sales Representative	Buenos Aires
+RATTC	Rattlesnake Canyon Grocery	Paula Wilson	Assistant Sales Representative	Albuquerque
+REGGC	Reggiani Caseifici	Maurizio Moroni	Sales Associate	Reggio Emilia
+RICAR	Ricardo Adocicados	Janete Limeira	Assistant Sales Agent	Rio de Janeiro
+RICSU	Richter Supermarkt	Michael Holz	Sales Manager	Genève
+ROMEY	Romero y tomillo	Alejandra Camino	Accounting Manager	Madrid
+SANTG	Santé Gourmet	Jonas Bergulfsen	Owner	Stavern
+SAVEA	Save-a-lot Markets	Jose Pavarotti	Sales Representative	Boise
+SEVES	Seven Seas Imports	Hari Kumar	Sales Manager	London
+SIMOB	Simons bistro	Jytte Petersen	Owner	Kobenhavn
+SPECD	Spécialités du monde	Dominique Perrier	Marketing Manager	Paris
+SPLIR	Split Rail Beer & Ale	Art Braunschweiger	Sales Manager	Lander
+SUPRD	Suprêmes délices	Pascale Cartrain	Accounting Manager	Charleroi
+THEBI	The Big Cheese	Liz Nixon	Marketing Manager	Portland
+THECR	The Cracker Box	Liu Wong	Marketing Assistant	Butte
+TOMSP	Toms Spezialitäten	Karin Josephs	Marketing Manager	Münster
+TORTU	Tortuga Restaurante	Miguel Angel Paolino	Owner	México D.F.
+TRADH	Tradição Hipermercados	Anabela Domingues	Sales Representative	Sao Paulo
+TRAIH	Trail's Head Gourmet Provisioners	Helvetius Nagy	Sales Associate	Kirkland
+VAFFE	Vaffeljernet	Palle Ibsen	Sales Manager	Århus
+VICTE	Victuailles en stock	Mary Saveley	Sales Agent	Lyon
+VINET	Vins et alcools Chevalier	Paul Henriot	Accounting Manager	Reims
+WANDK	Die Wandernde Kuh	Rita Müller	Sales Representative	Stuttgart
+WARTH	Wartian Herkku	Pirkko Koskitalo	Accounting Manager	Oulu
+WELLI	Wellington Importadora	Paula Parente	Sales Manager	Resende
+WHITC	White Clover Markets	Karl Jablonski	Owner	Seattle
+WILMK	Wilman Kala	Matti Karttunen	Owner/Marketing Assistant	Helsinki
+WOLZA	Wolski Zajazd	Zbyszek Piestrzeniewicz	Owner	Warszawa
+
+The products and categories tables
+
+Each product has a product_id and product_name. 
+  Each product belongs to one category (category_id) and has a fixed unit_price. 
+  The discontinued column holds either a false (available in the store) or a true (no longer available) for each product.
+
+Each category has an ID and a category_name. There is also a short description.
+
+Exercise
+The products table contains a category_id column, but no category_name column. 
+  To find out the category name for a given product, you will have to join these two tables.
+
+Show each product_name alongside its category_name.
+
+  select 
+	product_name,
+    category_name
+from products
+join categories
+using (category_id)
+
+product_name	category_name
+Chai	Beverages
+Chang	Beverages
+Aniseed Syrup	Condiments
+Chef Anton's Cajun Seasoning	Condiments
+Chef Anton's Gumbo Mix	Condiments
+Grandma's Boysenberry Spread	Condiments
+Uncle Bob's Organic Dried Pears	Produce
+Northwoods Cranberry Sauce	Condiments
+Mishi Kobe Niku	Meat/Poultry
+Ikura	Seafood
+Queso Cabrales	Dairy Products
+Queso Manchego La Pastora	Dairy Products
+Konbu	Seafood
+Tofu	Produce
+Genen Shouyu	Condiments
+Pavlova	Confections
+Alice Mutton	Meat/Poultry
+Carnarvon Tigers	Seafood
+Teatime Chocolate Biscuits	Confections
+Sir Rodney's Marmalade	Confections
+Sir Rodney's Scones	Confections
+Gustaf's Knäckebröd	Grains/Cereals
+Tunnbröd	Grains/Cereals
+Guaraná Fantástica	Beverages
+NuNuCa Nuß-Nougat-Creme	Confections
+Gumbär Gummibärchen	Confections
+Schoggi Schokolade	Confections
+Rössle Sauerkraut	Produce
+Thüringer Rostbratwurst	Meat/Poultry
+Nord-Ost Matjeshering	Seafood
+Gorgonzola Telino	Dairy Products
+Mascarpone Fabioli	Dairy Products
+Geitost	Dairy Products
+Sasquatch Ale	Beverages
+Steeleye Stout	Beverages
+Inlagd Sill	Seafood
+Gravad lax	Seafood
+Côte de Blaye	Beverages
+Chartreuse verte	Beverages
+Boston Crab Meat	Seafood
+Jack's New England Clam Chowder	Seafood
+Singaporean Hokkien Fried Mee	Grains/Cereals
+Ipoh Coffee	Beverages
+Gula Malacca	Condiments
+Rogede sild	Seafood
+Spegesild	Seafood
+Zaanse koeken	Confections
+Chocolade	Confections
+Maxilaku	Confections
+Valkoinen suklaa	Confections
+Manjimup Dried Apples	Produce
+Filo Mix	Grains/Cereals
+Perth Pasties	Meat/Poultry
+Tourtière	Meat/Poultry
+Pâté chinois	Meat/Poultry
+Gnocchi di nonna Alice	Grains/Cereals
+Ravioli Angelo	Grains/Cereals
+Escargots de Bourgogne	Seafood
+Raclette Courdavault	Dairy Products
+Camembert Pierrot	Dairy Products
+Sirop d'érable	Condiments
+Tarte au sucre	Confections
+Vegie-spread	Condiments
+Wimmers gute Semmelknödel	Grains/Cereals
+Louisiana Fiery Hot Pepper Sauce	Condiments
+Louisiana Hot Spiced Okra	Condiments
+Laughing Lumberjack Lager	Beverages
+Scottish Longbreads	Confections
+Gudbrandsdalsost	Dairy Products
+Outback Lager	Beverages
+Flotemysost	Dairy Products
+Mozzarella di Giovanni	Dairy Products
+Röd Kaviar	Seafood
+Longlife Tofu	Produce
+Rhönbräu Klosterbier	Beverages
+Lakkalikööri	Beverages
+Original Frankfurter grüne Soße	Condiments
+
+  The orders and order_items tables
+Very well done! Finally, we'll take a look at the orders and order_items tables.
+
+The orders table contains information about orders such as the order_id, customer_id, and the employee_id of the worker responsible for that order. 
+  There are also timestamp columns (order_date, required_date, and shipped_date) and a lot of columns related to the shipment process. 
+  Finally, there is the amount column, which shows the total amount paid for that order.
+
+The order_items table, in turn, stores information about what was actually ordered in a given order. 
+  Each row represents a single line item from an order. You can find the order_id, product_id, and the amount paid for the specific item.
+
+Exercise
+For each order display order_id, order_date, the number of products ordered in this order (name this column product_count) and the total amount paid for that order (name this column total_amount).
+
+To access the total_amount for each order, you should use the amount column from the orders table.
+
+select
+ 	order_id,
+    order_date,
+    count(product_id) as product_count,
+    sum(oi.amount) as total_amount
+from order_items oi
+join orders o
+using (order_id)
+group by order_id, order_date
+
+order_id	order_date	product_count	total_amount
+10323	2016-10-07	3	164.40
+11021	2023-01-03	5	6306.24
+10798	2017-12-26	2	446.60
+10346	2016-11-05	2	1618.88
+10711	2017-10-21	3	4451.70
+10908	2018-02-26	2	663.10
+10858	2018-01-29	3	649.00
+10422	2017-01-22	1	49.80
+10499	2017-04-08	2	1412.00
+10531	2017-05-08	1	110.00
+10943	2021-02-06	3	711.00
+10910	2018-02-26	3	452.90
+10645	2017-08-26	2	1535.00
+11046	2023-08-11	3	1485.80
+10417	2017-01-16	4	11188.40
+10940	2021-01-02	2	360.00
+10719	2017-10-27	3	844.25
+10483	2017-03-24	2	668.80
+10919	2020-04-21	3	1122.80
+10784	2017-12-18	3	1488.00
+10801	2017-12-29	2	3026.85
+10309	2016-09-19	5	1762.00
+10384	2016-12-16	2	2222.40
+10362	2016-11-25	3	1549.60
+10937	2020-12-01	2	644.80
+10995	2022-05-23	2	1196.00
+10445	2017-02-13	2	174.90
+10749	2017-11-20	3	1080.00
+11029	2023-04-12	2	1286.80
+10706	2017-10-16	3	1893.00
+10520	2017-04-29	2	200.00
+10459	2017-02-27	3	1659.20
+10712	2017-10-21	2	1233.48
+10928	2020-08-07	2	137.50
+10482	2017-03-21	1	147.00
+10875	2018-02-06	3	709.55
+10657	2017-09-04	6	4371.60
+11039	2023-06-27	4	3090.00
+10618	2017-08-01	3	2697.50
+10644	2017-08-25	3	1371.80
+10344	2016-11-01	2	2296.00
+10826	2018-01-12	2	730.00
+10787	2017-12-19	2	2622.76
+10279	2016-08-13	1	351.00
+10951	2021-04-14	3	458.77
+10449	2017-02-18	3	1838.20
+10561	2017-06-06	2	2844.50
+10356	2016-11-18	3	1106.40
+10378	2016-12-10	1	103.20
+10999	2022-07-04	3	1197.95
+10934	2020-10-18	1	500.00
+11066	2024-02-16	3	928.75
+10444	2017-02-12	4	1031.70
+10573	2017-06-19	3	2082.00
+10790	2017-12-22	2	722.50
+10655	2017-09-03	1	154.40
+10545	2017-05-22	1	210.00
+10809	2018-01-01	1	140.00
+11019	2022-12-24	2	76.00
+10893	2018-02-18	5	5502.11
+10867	2018-02-03	1	98.40
+10487	2017-03-26	3	889.70
+10941	2021-01-19	4	4011.75
+10927	2020-07-28	3	800.00
+10515	2017-04-23	5	9921.30
+10506	2017-04-15	2	415.80
+10382	2016-12-13	5	2900.00
+10558	2017-06-04	5	2142.90
+10415	2017-01-15	2	102.40
+10328	2016-10-14	3	1168.00
+11036	2023-05-20	2	1692.00
+10340	2016-10-29	3	2436.18
+10443	2017-02-12	2	517.44
+10391	2016-12-23	1	86.40
+10397	2016-12-27	2	716.72
+10640	2017-08-21	2	708.75
+10643	2017-08-25	3	814.50
+10595	2017-07-10	3	4725.00
+10373	2016-12-05	2	1366.40
+10650	2017-08-29	3	1779.20
+10505	2017-04-14	1	147.90
+10945	2021-02-25	2	245.00
+11027	2023-03-23	2	877.73
+10408	2017-01-08	3	1622.40
+10533	2017-05-12	3	2222.20
+10906	2018-02-25	1	427.50
+10315	2016-09-26	2	516.80
+10668	2017-09-15	3	625.28
+10843	2018-01-21	1	159.00
+10464	2017-03-04	4	1609.28
+10496	2017-04-04	1	190.00
+11011	2022-10-15	2	933.50
+10646	2017-08-27	4	1446.00
+10468	2017-03-07	2	717.60
+10436	2017-02-05	4	1994.52
+10511	2017-04-18	3	2550.00
+10252	2016-07-09	3	3597.90
+10386	2016-12-18	2	166.00
+10915	2018-02-27	3	539.50
+10963	2021-09-17	1	57.80
+
+----------------Revenue to date-----------------------------
+let's take a look at some basic revenue metrics.
+
+In this part we'll discuss the following revenue metrics:
+
+Total revenue to date – the total revenue from all orders. We'll take advantage of this metric to get to know the revenue columns in our database.
+Total revenue in a period of time – often we're interested in the total revenue for a specific period of time, e.g., for a specific month or a specific quarter.
+Total revenue for the current year, month, etc. – they show how much the company earned so far in the given current period. 
+             Commonly used report types are year-to-date, quarter-to-date, or month-to-date.
+The easiest metric to compute is the total revenue to date, i.e., the total revenue from all orders. 
+
+  Here is a SUM() function that will calculate it:
+
+SELECT
+  SUM(amount) AS total_revenue
+FROM orders;
+You could also use the order_items table to get the same result:
+
+SELECT
+  SUM(amount) AS total_revenue
+FROM order_items;
+As you can see, we only need to sum all values from the amount column of the orders or order_items tables to get the revenue-to-date value. We can also add a WHERE clause to find the revenue-to-date for specific orders, for instance:
+
+SELECT
+  SUM(amount) AS total_revenue
+FROM orders
+WHERE ship_country = 'USA';
+The query above finds the revenue to date for orders shipped to the USA.
+
+  Exercise
+Find the total revenue to date for all beverages (i.e., products with category_id = 1).
+  Use the amount column from order_items as total_revenue.
+  
+SELECT
+  SUM(amount) AS total_revenue
+FROM order_items
+JOIN products p
+USING (product_id)
+WHERE category_id = 1;
+
+total_revenue
+267868.20
+
+---   Revenue to date by group---------------------------
+Excellent! Now let us see how we can show the revenue generated by various groups to date. 
+  For instance:
+
+SELECT
+  c.customer_id,
+  c.company_name,
+  SUM(o.amount) AS total_revenue 
+FROM orders o
+JOIN customers c
+  ON o.customer_id = c.customer_id
+GROUP BY c.customer_id, c.company_name;
+The query above shows the revenue to date for each customer. To find the total amount for each customer, we group the whole query by the customer_id and company_name columns from the customers table.
+
+Exercise
+Show each category_id and category_name alongside the total_revenue generated by all order items from that category.
+
+SELECT
+  c.category_id,
+c.category_name,
+  SUM(oi.amount) AS total_revenue 
+FROM order_items oi
+JOIN products p
+using (product_id)
+JOIN categories c
+using (category_id)
+GROUP BY c.category_id,c.category_name
+
+category_id	category_name	total_revenue
+4	Dairy Products	234507.32
+6	Meat/Poultry	163022.38
+2	Condiments	106047.15
+3	Confections	167357.29
+5	Grains/Cereals	95744.60
+7	Produce	99984.58
+1	Beverages	267868.20
+8	Seafood	131261.77
 
