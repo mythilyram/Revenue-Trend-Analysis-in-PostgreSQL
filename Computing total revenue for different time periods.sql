d155bba995d50813d1c32b872f6ccb6ddbb62e15
Mythily Ramanathan <123518126+mythilyram@users.noreply.github.com>
2024-04-04 20:05:27 -0400

    Update Computing total revenue for different time periods.sql

--- Computing total revenue for different time periods.sql
+++ Computing total revenue for different time periods.sql
@@ -1,1 +1,21 @@
 
+Welcome
+  
+Hello and welcome to our PostgreSQL Revenue Trend Analysis course!
+  This course is aimed for students from various industries who would like to use PostgreSQL to analyze revenue trends for their companies. 
+  Once you learn how to analyze revenue, you'll also be able to analyze other trends, such as website visits over time, in a similar way.
+
+Revenue is the income generated from the sale of products (or services, if that's what your company is selling), before any costs or expenses are deducted. 
+  It is sometimes called sales or (in the UK) turnover. To determine net income you subtract all charges, costs, and expenses from the revenue.
+
+Revenue is the most important figure in a financial statement for a company. In this course we'll look at various revenue metrics and we'll learn how to compute them in SQL.
+
+SQL Basics - JOIN, WHERE, GROUP BY, ORDER BY, and HAVING clauses – all of which you'll need for this course.
+
+First, we'll get to know the database we're going to work with.
+
+The Northwind database
+We'll be using a Microsoft database named Northwind. 
+  It describes a fictional store, its customers, and its orders. The original database is available here. 
+  We've modified it a bit to better suit the needs of this course.
+
